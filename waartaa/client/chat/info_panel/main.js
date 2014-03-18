06fb1a4931765c31ef10663aca74aa9dd2f563e7
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2014-03-18 23:10:19 +0200

    Catch the event of changing the content of the search box (#70)

--- waartaa/client/chat/info_panel/main.js
+++ waartaa/client/chat/info_panel/main.js
@@ -1,3 +1,9 @@
 Template.info_panel_body.rendered = function () {
   $('#info-panel .nano').nanoScroller();
 }
+Template.info_panel_header.events = {
+  'keyup #users-search': function (e) {
+    var searchKey = $('#users-search').val();
+    Session.set ('searchKey', searchKey);
+  }
+}
